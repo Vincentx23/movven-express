5d56d45b7b7da9629fd0f36521c0b3a3329eec25
maitcsan23 <macuervo84@gmail.com>
2020-09-11 03:45:18 -0500

    Adicion de modulo de axios, funcion de login hecha

--- src/components/LoginForm.svelte
+++ src/components/LoginForm.svelte
@@ -1,25 +1,57 @@
+<script>
+  import Axios from "axios";
 
+  let user = {
+    email: "",
+    password: "",
+  };
+
+  const login = async (e) => {
+    e.preventDefault();
+    try {
+      const loginRes = await Axios.post(
+        "http://localhost:8080/api/auth/login",
+        user
+      );
+
+      localStorage.setItem("x-access-token", loginRes.data.token);
+      window.location.replace('/Dashboard')
+    } catch (err) {
+        console.log(err.response.data.message)
+        err.response.data.message  
+    }
+  };
+</script>
 
 <style>
 </style>
 
-
 <div class="registration-form">
-    <form>
-        <div class="form-icon">
-            <span><i class="icon icon-user"></i></span>
-          <!-- <img src="./img/Movven-Logo.png" alt="Movven-Logo"> -->
-        </div>
-        <div class="form-group">
-            <input type="text" class="form-control item" id="email" placeholder="Nombre de usuario o correo electrónico">
-        </div>
-        <div class="form-group">
-            <input type="password" class="form-control item" id="password" placeholder="Contraseña">
-        </div>
-        <div class="form-group">
-           
-              <button type="button" class="btn btn-block create-account" onclick="redirection()" >Acceder</button>
-           
-        </div>
-    </form>
+  <form on:submit={login}>
+    <div class="form-icon">
+      <span><i class="icon icon-user" /></span>
+      <!-- <img src="./img/Movven-Logo.png" alt="Movven-Logo"> -->
+    </div>
+    <div class="form-group">
+      <input
+        id="email"
+        bind:value={user.email}
+        type="text"
+        class="form-control item"
+        placeholder="Nombre de usuario o correo electrónico" />
+    </div>
+    <div class="form-group">
+      <input
+        bind:value={user.password}
+        type="password"
+        class="form-control item"
+        id="password"
+        placeholder="Contraseña" />
+    </div>
+    <div class="form-group">
+      <button
+        type="submit"
+        class="btn btn-block create-account">Acceder</button>
+    </div>
+  </form>
 </div>

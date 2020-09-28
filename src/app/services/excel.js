51f43e968be4fa73a1f9159ebead376fde042ffa
maitcsan23 <macuervo84@gmail.com>
2020-09-27 23:25:31 -0500

    Subida de datos de excel añadido y ajuste de redireccion de opcion de pedidos

--- src/app/services/excel.js
+++ src/app/services/excel.js
@@ -8,9 +8,9 @@ module.exports = {
         return new Promise((resolve, reject) => {
             readXlsxFile(file).then((rows) => {
                 rows.shift();
-                const values = rows.map((row) => [...row.slice(0, -1), userId]);
+                const values = rows.map((row) =>  [...row.slice(0), userId]);
 
-                const keys = ['codeDelivery', 'clientName', 'state', 'distric', 'directionDetails', 'phone', 'orderDescription', 'amountPakages', 'totalDimensions', 'limitDate', 'userId'];
+                const keys = ['codeDelivery', 'clientName', 'state', 'city', 'distric', 'directionDetails', 'phone', 'orderDescription', 'amountPakages', 'totalDimensions', 'limitDate', 'payment', 'userId'];
 
                 if(values.length === 0) {
                     reject(['E503', new Error("Excel file haven't data")])
@@ -31,31 +31,33 @@ module.exports = {
         const worksheet = workbook.addWorksheet("Plantilla");
 
         const template = Array(10).fill(1).map(() => ({
-            codeDelivery: 123,
+            codeDelivery: 'ABCSJDS',
             clientName: 'Christhoval Barba',
-            state: 'Panama',
-            Distric: 'San Miguelito',
-            Directiondetail: 'Los Andes, calle El Lago.',
+            state: 1,
+            city: 'Panama',
+            distric: 'San Miguelito',
+            directionDetails: 'Los Andes, calle El Lago.',
             phone: '61023295',
-            Orderdescription: 'lala lala lala lala lala lala lala',
-            AmountPackage: 12,
-            Totaldimension: 123,
-            Limitdate: '2020/09/24',
-            total: 10.99,
+            orderDescription: 'lala lala lala lala lala lala lala',
+            amountPakages: 12,
+            totalDimensions: '12 x 12',
+            limitDate: '2020/09/24',
+            payment: 10,
         }));
 
         worksheet.columns = [
             {header: "ID", key: "codeDelivery", width: 5},
             {header: "NOMBRE COMPLETO", key: "clientName", width: 25},
-            {header: "PROVINCIA", key: "state", width: 25},
-            {header: "DISTRITO", key: "Distric", width: 25},
-            {header: "DIRECCION(ZONA, NUMERO DE CASA, CALLE)", key: "Directiondetail", width: 10},
+            {header: "ESTADO DE ENTREGA", key: "state", width: 25},
+            {header: "CIUDAD", key: "city", width: 25},
+            {header: "DISTRITO", key: "distric", width: 25},
+            {header: "DIRECCION(ZONA, NUMERO DE CASA, CALLE)", key: "directionDetails", width: 10},
             {header: "CELULAR", key: "phone", width: 25},
-            {header: "DESCRIPCION DE ENTREGA", key: "Orderdescription", width: 10},
-            {header: "CANTIDAD DE PAQUETES", key: "AmountPackage", width: 10},
-            {header: "PESO (LB)", key: "Totaldimension", width: 10},
-            {header: "LIMITE DE TIEMPO PARA ENTREGAR", key: "Limitdate", width: 10},
-            {header: "PAGO TOTAL", key: "total", width: 10},
+            {header: "DESCRIPCION DE ENTREGA", key: "orderDescription", width: 10},
+            {header: "CANTIDAD DE PAQUETES", key: "amountPakages", width: 10},
+            {header: "PESO (LB)", key: "totalDimensions", width: 10},
+            {header: "LIMITE DE TIEMPO PARA ENTREGAR", key: "limitDate", width: 10},
+            {header: "PAGO TOTAL", key: "payment", width: 10},
         ];
 
         worksheet.addRows(template);
--- src/app/utils/db.js
+++ src/app/utils/db.js
@@ -2,9 +2,11 @@ const db = require("../../database");
 
 function bulkInsert(table, keys, values) {
     const query = `INSERT INTO ${table} (${keys.join(',')}) VALUES ?`;
-
+    console.log(keys);
     return new Promise((resolve, reject) => {
         db.query(query, [values], function(err, result) {
+            console.log(values);
+
             if(err){
                 reject(err);
             }

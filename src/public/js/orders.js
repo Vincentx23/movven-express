eaab908f759e73e9e6aa1421020e7a68edd3a957
Vincentx23 <46614369+Vincentx23@users.noreply.github.com>
2020-09-27 18:33:19 -1200

    Cambios en pantallas de dashboar de pedidos

--- src/public/js/orders.js
+++ src/public/js/orders.js
@@ -98,7 +98,7 @@ $(document).ready(function () {
                         state = 'Rechazado'
                     }
                     $('#staticBackdrop').empty()
-                    $('#staticBackdrop').append(generateModal(order.createdAt, order.codeDelivery, order.clientName, order.phone, order.city, order.distric,state))
+                    $('#staticBackdrop').append(generateModal(order.createdAt, order.codeDelivery, order.city, order.distric, order.directionDetails,order.limitDate,order.amountPakages, order.totalDimensions))
                 })
             },
             error: function (xhr, status, error) {
@@ -121,41 +121,90 @@ $(document).ready(function () {
      * @param {*} city 
      * @param {*} distric 
      * @param {*} state 
+     * @param {*} orderDescription
+     * @param {*} limitDate
      */
 
-    function generateTableOrdersBodyTemplate(createdAt, idDelivery, clientName, phone, city,distric, state) {
+    function generateTableOrdersBodyTemplate(createdAt, idDelivery, clientName, phone, city,distric, state, orderDescription) {
         return '<tr>\n' +
             '<td>'+ createdAt +'</td>\n' +
             '<td id="td-codeDelivery" name="td-codeDelivery">'+idDelivery+'</td>\n' +
             '<td>'+clientName+'</td>\n' +
             ' <td>'+phone+'</td>\n' +
            // '<td>'+city+'</td>\n' +
-            '<td>'+distric+'</td>\n' +
+            '<td>'+orderDescription+'</td>\n' +
             '<td>'+state+'</td>\n' +
-            '<td> <button type="button" id="btnDetalle" data-id="'+idDelivery+'" class="btn btn-primary" data-toggle="modal" data-target="#staticBackdrop"> Mas detalles </button></td>'+
+            '<td> <button type="button" id="btnDetalle" data-id="'+idDelivery+'" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#staticBackdrop"><i class="fas fa-eye"></i> Más detalles</button></td>'+
             '</tr>';
         
 
     }
 
-    function generateModal(createdAt,codeDelivery, clientName, phone, city, distric,state){
+    function generateModal(createdAt,codeDelivery, city, distric, directionDetails, limitDate, amountPakages, totalDimensions, phone,state, clientName){
         // Modal 
     return'<div class="modal-dialog">\n' +
         '<div class="modal-content">\n' +
         '<div class="modal-header">\n' +
-            '<h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>\n' +
+            '<h5 class="modal-title" id="staticBackdropLabel">Detalles</h5>\n' +
             '<button type="button" class="close" data-dismiss="modal" aria-label="Close">\n' +
             '<span aria-hidden="true">&times;</span>\n' +
             '</button>\n' +
         '</div>\n' +
         '<div class="modal-body">\n' +
-        createdAt +
+        '<div class="container">\n' +
+            '<div class="row">\n' +
+                '<div class="col-sm">\n' +
+                     //iddelivery
+                    '<h6 class="m-0 font-weight-bold movven-color"><i class="fas fa-barcode"></i> ID Delivery </h6>\n' +codeDelivery +
+                '</div>\n' +
+                '<div class="col-sm">\n' +                  
+                    //Provincia
+                     '<h6 class="m-0 font-weight-bold movven-color"><i class="fas fa-map-signs"></i> Provincia y Distrito</h6>\n' +city +', '+ distric + 
+                '</div>\n' +
+            
+            '</div>\n' + 
+         
+            '<div class="row">\n' +            
+                '<div class="col-sm">\n' +
+                '<br>\n' + 
+                     //Direccion
+                    '<h6 class="m-0 font-weight-bold movven-color"><i class="fas fa-map"></i> Dirección </h6>\n' +directionDetails +
+                '</div>\n' + 
+                '<div class="col-sm">\n' + 
+                '<br>\n' +    
+                    //Límite de tiempo para entrega.
+                    '<h6 class="m-0 font-weight-bold movven-color"><i class="fas fa-clock"></i> Límite de tiempo para entrega</h6>\n' +limitDate  +
+                '</div>\n' + 
+            '</div>\n' + 
+            '<div class="row">\n' +            
+                '<div class="col-sm">\n' +
+                '<br>\n' +  
+                    //Cantidad de paquetes
+                    '<h6 class="m-0 font-weight-bold movven-color"><i class="fas fa-cubes"></i> Cantidad de paquetes</h6>\n' +amountPakages+
+                        // Modal +
+                '</div>\n' + 
+                '<div class="col-sm">\n' + 
+                '<br>\n' +    
+                    // Peso y dimensiones
+                    '<h6 class="m-0 font-weight-bold movven-color"><i class="fas fa-weight-hanging"></i> Peso y dimensiones</h6>\n'+'14.78 lb'+', ' +totalDimensions+
+                '</div>\n' + 
+
+            '</div>\n' +   
+            '<div class="row">\n' +            
+                '<div class="col-sm">\n' +
+                '<br>\n' +  
+                    //Pago total
+                    '<h6 class="m-0 font-weight-bold movven-color"><i class="fas fa-money-bill"></i> Pago total</h6>\n' +'12.56 $'+       
+                '</div>\n' +  
+            '</div>\n' +
+        '</div>\n' +
+
 
 
         '</div>\n' +
         '<div class="modal-footer">\n' +
-            '<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>\n' +
-            '<button type="button" class="btn btn-primary">Understood</button>\n' +
+            '<button type="button" class="btn btn-outline-dark" data-dismiss="modal">Cerrar</button>\n' +
+          
     '</div>\n' +
     '</div>\n' +
     '</div>\n';
@@ -195,7 +244,7 @@ $(document).ready(function () {
                     } else {
                         state = 'Rechazado'
                     }
-                    $('#tbody-orders').append(generateTableOrdersBodyTemplate(order.createdAt, order.codeDelivery, order.clientName, order.phone, order.city, order.distric,state))
+                    $('#tbody-orders').append(generateTableOrdersBodyTemplate(order.createdAt, order.codeDelivery, order.clientName, order.phone, order.city, order.distric,state, order.orderDescription))
                 })
             },
             error: function (xhr, status, error) {
